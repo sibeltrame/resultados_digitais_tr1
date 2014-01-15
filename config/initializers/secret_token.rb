fc9aa9161b11eae979d8f1f13ab9cafce3e9c9db
sibeltrame <sibeltrame@gmail.com>
2014-01-14 22:38:31 -0200

    first ultra big fast danger commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResultadoDigitaisTr1::Application.config.secret_key_base = '6ef79a2f33cabe0e80a777b407cc8139a4852c7166200f63370bd2727ae607e8ef52b20939b531f05d40d956c9e229874a46edc5704802e358ea496ef5d719c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ResultadoDigitaisTr1::Application.config.session_store :cookie_store, key: '_resultado_digitais_tr1_session'
